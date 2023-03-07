d01a400dcff5b53fb412f7e8961089cae1116c33
Harsh Vardhan <23harshvardhan@gmail.com>
2023-03-07 18:59:53 +0530

    Downloads updates and replaces file if new version is found on cloud.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HRSH_Transpera_Updater.Tools;
+using System;
+using System.Diagnostics;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace HRSH_Transpera_Updater
 {
@@ -23,6 +15,59 @@ namespace HRSH_Transpera_Updater
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void mainWind_Loaded(object sender, RoutedEventArgs e)
+        {
+            btnFinish.IsEnabled = false;
+            //btnUpdate.IsEnabled = false;
+
+            lblStatus.Content = "Checking for updates...";
+
+            IniFile config = new IniFile(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"\HRSH\Transpera\config.ini");
+            string currentVersion = config.Read("Version", "Settings");
+
+            WebClient client = new WebClient();
+            string latestVersion = client.DownloadString("https://an0maly.blob.core.windows.net/transpera/version.txt");
+
+            if(currentVersion != latestVersion)
+            {
+                lblStatus.Content = "Updates Found!";
+                btnUpdate.IsEnabled = true;
+            }
+            else if(currentVersion == latestVersion)
+            {
+                lblStatus.Content = "No updates found.";
+                btnFinish.IsEnabled = true;
+            }
+            else
+            {
+                lblStatus.Content = "Error! Reinstall.";
+                btnFinish.IsEnabled = true;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            btnUpdate.IsEnabled = false;
+
+            foreach (var process in Process.GetProcessesByName("HRSH-Transpera"))
+            {
+                process.Kill();
+            }
+
+            File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\HRSH\Transpera\HRSH-Transpera.exe");
+
+            WebClient client = new WebClient();
+            client.DownloadFile("https://an0maly.blob.core.windows.net/transpera/HRSH-Transpera.exe", Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\HRSH\Transpera\HRSH-Transpera.exe");
+
+            lblStatus.Content = "Updated!";
+            btnFinish.IsEnabled = true;
+        }
+
+        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
